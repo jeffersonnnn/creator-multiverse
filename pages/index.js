9ce80bf416590ea8e12e9d4cd8c79f4bf0ee6036
jeffersonnnn <jefferie.halo@gmailcom>
2022-12-15 16:01:59 +0100

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Mint from "../components/Mint";
+import Header from "../components/header";
+import Mint from "../components/mint";
 import "animate.css";
 
 export default function Home() {
